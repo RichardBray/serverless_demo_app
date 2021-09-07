7935eeca64b098bddb12f01c91f4475e389f24bc
Richard Bray <richardbray89@gmail.com>
2021-09-07 19:38:33 +0100

    chore: add s3 and dynamoDB

---
+++ src/create.js
@@ -0,0 +1,35 @@
+import * as uuid from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.TABLE_NAME,
+    Item: {
+      // The attributes of the item to be created
+      userId: '123', // The id of the author
+      noteId: uuid.v1(), // A unique uuid
+      content: data.content, // Parsed from request body
+      attachment: data.attachment, // Parsed from request body
+      createdAt: Date.now(), // Current Unix timestamp
+    },
+  };
+
+  try {
+    await dynamoDb.put(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: e.message }),
+    };
+  }
+}
